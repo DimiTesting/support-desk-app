b897bc9a32016a22b180c6cea9f19d062445ad01
amankeldi.kurban <dimitesting31@gmail.com>
2024-04-28 12:18:21 +0300

    adding ErrorHandlerMiddleware

--- backend/contollers/userControllers.js
+++ backend/contollers/userControllers.js
@@ -1,6 +1,20 @@
+
+// @desc   Register a new user
+// @route  /api/users
+// @access Public
+
 const registerUser = (req,res) => {
+    const {name, email, password} = req.body
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error("One of the fields are missing")
+    }
     res.send("Register User")
 }
+
+// @desc   Register a new user
+// @route  /api/users/login
+// @access Public
 
 const loginUser = (req,res) => {
     res.send("Login User")
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT ||5000
+const {errorHandler} = require('./middlewares/errorMiddleware')
 
 const app = express()
 
@@ -8,7 +9,11 @@ app.get("/", (req, res) => {
     res.status(200).json({message: "Welcome to support desk API app"})
 })
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
