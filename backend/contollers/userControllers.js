939e1718e812773261a97f4b23b113f9c003ab98
amankeldi.kurban <dimitesting31@gmail.com>
2024-04-28 12:31:24 +0300

    adding asyncHanlder package

--- backend/contollers/userControllers.js
+++ backend/contollers/userControllers.js
@@ -1,23 +1,24 @@
+const asyncHandler = require("express-async-handler")
 
 // @desc   Register a new user
 // @route  /api/users
 // @access Public
 
-const registerUser = (req,res) => {
+const registerUser = asyncHandler( async(req,res) => {
     const {name, email, password} = req.body
     if (!name || !email || !password) {
         res.status(400)
         throw new Error("One of the fields are missing")
     }
     res.send("Register User")
-}
+})
 
 // @desc   Register a new user
 // @route  /api/users/login
 // @access Public
 
-const loginUser = (req,res) => {
+const loginUser = asyncHandler(async(req,res) => {
     res.send("Login User")
-}
+})
 
 module.exports = {registerUser, loginUser}
