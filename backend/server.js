23f80eed5c2c99933251681b62d1a8502194a75c
amankeldi.kurban <dimitesting31@gmail.com>
2024-04-28 11:41:34 +0300

    settingUp routes and controller methods

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Server...")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT ||5000
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Welcome to support desk API app"})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
