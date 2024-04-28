fe9b37c06163594f58e9d8725786c24098326480
amankeldi.kurban <dimitesting31@gmail.com>
2024-04-28 12:56:18 +0300

    setting up connection with the DataBase

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT ||5000
+const colors = require('colors')
+const connectDB = require('./config/db')
 const {errorHandler} = require('./middlewares/errorMiddleware')
+
+//connecting to the database
+connectDB()
 
 const app = express()
 
