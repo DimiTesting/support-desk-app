990e8d6adb15076929b2dc6c52cd2e4ebe925163
amankeldi.kurban <dimitesting31@gmail.com>
2024-05-13 18:38:30 +0300

    setting up production with Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT ||8000
@@ -10,15 +11,25 @@ connectDB()
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.status(200).json({message: "Welcome to support desk API app"})
-})
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get("/", (req, res) => {
+        res.status(200).json({message: "Welcome to support desk API app"})
+    })
+    
+}
+
 app.use(errorHandler)
 
 app.listen(PORT, ()=> {
